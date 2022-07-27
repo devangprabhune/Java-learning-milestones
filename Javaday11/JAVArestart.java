951ee4743e0ec28b01cb6a7ed889c23a464900ef
devangprabhune <85066100+devangprabhune@users.noreply.github.com>
2022-07-27 17:44:33 +0530

    Add files via upload

---
+++ Javaday11/JAVArestart.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class JAVArestart {
+    public static void main(String[] args) {
+        // Scanner A = new Scanner(System.in);
+        // int PrAmt = A.nextInt();
+        // float RoItr = A.nextFloat();
+        // int time = A.nextInt();
+
+        // float R = (PrAmt*RoItr*time)/100;
+        // int result = (int) R;
+        // System.out.println(result);
+
+        //bitwise operation
+        // int num1 = 10;
+        // int num2 = -11;
+        // System.out.println(num1 + "'s bitwise complement is "+ ~num1);
+        // System.out.println(num2+"'s bitwise complement is "+ ~num2);
+
+        // System.out.println(num1 == ~num2);
+        // System.out.println("inshort, a number's complement is -(num+1)");
+
+        int a = 6, b = 7;
+        System.out.println("a|b is " + (a|b));
+        System.out.println("~a is "+(~a));
+
+        int c = 8;
+        System.out.println("c>>2 = "+(c>>2));
+        
+
+
+
+
+    }
+}
